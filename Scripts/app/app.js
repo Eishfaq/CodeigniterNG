0c466a9d8ca97bf7f1c60803440752a2f12bb152
DESKTOP-VEF6JPF\Eishfaq Ahmed Shafa <eaeishfaq@gmail.com>
2017-03-21 23:43:13 +0600

    First upload codeigniterNg

---
+++ Scripts/app/app.js
@@ -0,0 +1,63 @@
+﻿var app = angular.module('myApp', ['ngRoute', 'ngResource']);
+
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/home', { templateUrl: 'templates/list.html', controller: 'HomeCtrl' });
+    $routeProvider.when('/edit/:id', { templateUrl: 'templates/edit.html', controller: 'EditCtrl' });
+    $routeProvider.when('/create', { templateUrl: 'templates/create.html', controller: 'CreateCtrl' });
+    $routeProvider.otherwise({ redirectTo: '/home' });
+}]);
+
+
+app.controller('HomeCtrl', ['$scope','Profiles','$route',function($scope,Profiles,$route){
+	Profiles.get(function(data){
+		$scope.profiles = data.response;
+	});
+
+	$scope.remove = function(id){
+		Profiles.delete({id:id}).$promise.then(function(data){
+			if(data.response){
+				$route.reload();
+				$scope.success = "Item has been deleted.";
+			}
+		});
+	}
+}]);
+
+
+app.controller('EditCtrl', ['$scope','Profiles','$routeParams',function($scope,Profiles,$routeParams){
+	var id = $routeParams.id;
+
+	Profiles.get({id:id},function(data){
+		$scope.profiles = data.response;
+	});
+
+	$scope.submit = function(){
+		Profiles.update({id:$scope.profiles.id},{profile:$scope.profiles},function(data){
+			$scope.success = "Successfully Edited";
+		});
+	}
+}]);
+
+app.controller('CreateCtrl', ['$scope','Profiles',function($scope,Profiles){
+	$scope.profiles = {
+		name:"",
+		designation:"",
+		company:""
+	};
+
+	$scope.submit = function(){
+		Profiles.save({profile:$scope.profiles}).$promise.then(function(data){
+			if(data.response){
+				angular.copy({},$scope.profiles);
+				$scope.success = "Successfully Created.";
+			}
+		})
+	}
+}]);
+
+
+app.factory('Profiles',['$resource',function($resource){
+	return $resource('http://localhost/ng_tutorial/Api/:id',{id:'@_id'},{
+		update:{method:'PUT',params:{id:'@_id'}}
+	});
+}]);
---
+++ Scripts/app/controller.js
@@ -0,0 +1,83 @@
+﻿//factories
+
+
+
+app.factory('Student', ['$resource', function ($resource) {
+    var resource = $resource('http://localhost:1670/Api/Student');
+    return resource;
+}]);
+
+
+//controllers
+
+app.controller('myController1', ['$scope', 'Student', function($scope, Student) {
+
+    $scope.Heading = "Student List";
+    $scope.students = [];
+
+    Student.get(function(response) {
+        console.log(response);
+        $scope.students = response.Data;
+    });
+}])
+    .controller('myController2', ['$scope', 'Student', '$location', function($scope, Student, $location) {
+        $scope.title = "Add Students";
+        $scope.save = function() {
+            Student.save({ Name: $scope.Name, Address: $scope.Address, Phone: $scope.Phone }, function(response) {
+
+
+                if (response) {
+                    $scope.Notification = "Student is saved.";
+                    $scope.Name = "";
+                    $scope.Address = "";
+                    $scope.Phone = "";
+                    $location.path("/partial1");
+                } else {
+                    $scope.Notification = "Student is not saved!";
+                }
+
+            });
+        };
+    }])
+    .controller('myController3', ['$scope', 'Student', '$routeParams', '$location', function($scope, Student, $routeParams, $location) {
+        var requestedId = $routeParams.id;
+        $scope.Heading = "Remove a Student";
+
+        Student.get({ request: JSON.stringify(requestedId) }, function(response) {
+            //console.log(response);
+            $scope.student = response.Data;
+        });
+
+        $scope.delete = function() {
+            Student.remove({ request: JSON.stringify(requestedId) }, function(response) {
+                if (response.IsSuccess) {
+                    $location.path('/partial1');
+                }
+            });
+        };
+    }]);
+app.controller('myController4', ['$scope', 'Student', '$routeParams', '$location', function($scope, Student, $routeParams, $location) {
+
+    $scope.heading = "Update Information";
+
+    var requestedId = $routeParams.id;
+
+        Student.get({ response: JSON.stringify(requestedId) }, function(response) {
+            console.log(response);
+            $scope.student = response.Data;
+        });
+
+        $scope.save = function() {
+            Student.save($scope.student, function(response) {
+                if (response) {
+                    $scope.notification = "Saved";
+                    $scope.student = {};
+                    $location.path('/partial1');
+                    
+                } else {
+                    $scope.message = "Failed!";
+                }
+            });
+        };
+    }]);
+
